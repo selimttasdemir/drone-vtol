0051d631f49b30b000d696807f7695ee46109b12
Test Test <test@test.com>
2023-02-16 21:42:58 +0300

    initial commit

---
+++ pyMav/Baglanti.py
@@ -0,0 +1,52 @@
+"""
+Selim Taşdemir
+github.com/selimttasdemir
+Tarih : 02.14.2023
+
+Açıklama : 
+Bu dosyada IHA'nın mavlink ile bağlantı kurması
+mesajların filtrelenmesi işlemleri yapılmıştır
+
+"""
+
+
+
+from pymavlink import mavutil
+import serial
+
+adres = "udpin:localhost:14550"
+# adres = serial.Serial('/dev/ttyACM0')
+# adres = "/dev/ttyTHS1:115200"
+
+baglanti = mavutil.mavlink_connection(adres, baud=115200, autoreconnect=True)
+baglanti.wait_heartbeat()
+print("Baglanti kuruldu")
+
+# while True:
+#     mesaj = baglanti.recv_match() # Tüm mesajları al
+#     if mesaj is not None: # mesaj varsa
+#         print(mesaj) # mesajı yazdır
+
+
+mesajPil = baglanti.recv_match(type='BATTERY_STATUS', blocking=True) # type ile sadece istenen mesajı al
+if mesajPil is not None: # mesaj varsa
+    print(f"Batarya Yüzdesi: {mesajPil.battery_remaining}") # istenen mesajın istenen özelliğinin mesajını yazdır
+
+mesajRollPitch = baglanti.recv_match(type='AHRS2', blocking=True) # type ile sadece istenen mesajı al
+if mesajRollPitch is not None: # mesaj varsa
+    print(f"Roll: {mesajRollPitch.roll}, Pitch: {mesajRollPitch.pitch}") # istenen mesajın istenen özelliğinin mesajını yazdır
+
+mesajGSpeed = baglanti.recv_match(type='VFR_HUD', blocking=True) # type ile sadece istenen mesajı al
+if mesajGSpeed is not None: # mesaj varsa
+    print(f"Ground Speed: {mesajGSpeed.groundspeed}") # istenen mesajın istenen özelliğinin mesajını yazdır
+
+mesajASpeed = baglanti.recv_match(type='VFR_HUD', blocking=True) # type ile sadece istenen mesajı al
+if mesajASpeed is not None: # mesaj varsa
+    print(f"Air Speed: {mesajASpeed.airspeed}") # istenen mesajın istenen özelliğinin mesajını yazdır
+
+# Stabilize modunu yazdır
+mesajMod = baglanti.recv_match(type='HEARTBEAT', blocking=True) # HEARTBEAR ile IHA'nın mode durumunu öğren
+mode = mavutil.mode_string_v10(mesajMod)
+if mesajMod is not None: # mesaj varsa
+    print(f"Mod: {mode}") # istenen mesajın istenen özelliğinin mesajını yazdır
+
---
+++ pyMav/takeoff_mode.py
@@ -0,0 +1,84 @@
+# coding=utf-8
+from pymavlink import mavutil
+import serial , time
+
+adres = "udpin:localhost:14550"
+# adres = serial.Serial('/dev/ttyACM0')
+# adres = "/dev/ttyTHS1:115200"
+
+VTOLbrkt = mavutil.mavlink_connection(adres, baud=115200, autoreconnect=True)
+VTOLbrkt.wait_heartbeat()
+print("Baglanti kuruldu")
+
+VTOLbrkt.set_mode("GUIDED")
+print("GUIDED moduna gecildi")
+
+VTOLbrkt.arducopter_arm() # IHA'yý arm et (kalkýþ yapýlabilir hale getir) fakat yalnýzca GUIDED modunda çalýþýr ve droneler için kullanýlýr
+print("Arm edildi")
+
+VTOLbrkt.motors_armed() # ayrýca uçak yada VTOL ýhalar kullanýlacaksa ekstradan bu komutu yazmak gereklidir. Çünkü bu komutlar arasýnda bir fark vardýr. 
+# "motors_armed()"" komutu sadece motorlarý arm eder. Ancak "arducopter_arm()" komutu motorlarý arm eder ve ayný zamanda kalkýþ yapýlabilir hale getirir.
+
+VTOLbrkt.motors_armed_wait() # motorlar arm olana kadar bekler
+print("Motorlar arm edildi")
+
+def anlik_yukseklik():
+    mesaj = VTOLbrkt.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
+    alt = mesaj.relative_alt/1000
+    return alt
+
+# takeoff komutu ile IHA'nýn kalkýþ yapmasý saðlanýr
+# takeoff komutu 2 parametre alýr
+# 1. parametre: kalkýþ yapýlacak yükseklik
+# 2. parametre: kalkýþ yapýlacak hýz
+def takeoff(yukseklik, hiz):
+    VTOLbrkt.mav.command_long_send(VTOLbrkt.target_system, VTOLbrkt.target_component, mavutil.mavlink.MAV_CMD_NAV_VTOL_TAKEOFF, 0,0,0,0,0,0,0,yukseklik, hiz)
+    print("Kalkis yapiliyor")
+    VTOLbrkt.motors_armed_wait() # motorlar arm olana kadar bekler
+    print("Kalkis yapildi")
+    
+
+
+
+    while True:
+        if anlik_yukseklik() >= yukseklik:
+            print("Kalkis tamamlandi")
+            break
+
+takeoff(10, 0) # 10 metre yükseklikte 0 hýzla kalkýþ yap
+
+# MAV_CMD_NAV_VTOL_TAKEOFF komutu ile IHA'nýn kalkýþ yapmasý saðlanýr
+# MAV_CMD_NAV_VTOL_TAKEOFF komutu 11 parametre alýr
+# 1. parametre: 0
+# 2. parametre: 0
+# 3. parametre: 0
+# 4. parametre: 0
+# 5. parametre: 0
+# 6. parametre: 0
+# 7. parametre: 0
+# 8. parametre: 10 (kalkýþ yapýlacak yükseklik)
+# 9. parametre: 0 (kalkýþ yapýlacak hýz)
+# 10. parametre: 0 (kalkýþ yapýlacak hýz)
+# yazýlan rakamlardan bir tanesi onaylama için olmak üzere 7tane 0 yazýldý ardýndan yukseklik ve hýz parametreleri yazýldý. 0,0,0,0,0,0,10,0 yazýlabilirdi.
+
+VTOLbrkt.motors_armed_wait() # motorlar arm olana kadar bekler
+print("Motorlar arm edildi")
+
+# takeoff komutu ile IHA'nýn kalkýþ yapmasý saðlanýr
+# takeoff komutu 2 parametre alýr
+# 1. parametre: kalkýþ yapýlacak yükseklik
+# 2. parametre: kalkýþ yapýlacak hýz
+
+time.sleep(10) # 10 saniye bekle
+
+def land(yukseklik, hiz):
+    VTOLbrkt.mav.command_long_send(VTOLbrkt.target_system, VTOLbrkt.target_component, mavutil.mavlink.MAV_CMD_NAV_VTOL_LAND, 0,0,0,0,0,0,0,yukseklik,hiz)
+    print("Inis yapiliyor")
+    VTOLbrkt.motors_armed_wait() # motorlar arm olana kadar bekler
+    print("Inis yapildi")
+    VTOLbrkt.arducopter_disarm() # IHA'yý disarm et (kalkýþ yapýlamaz hale getir)
+    print("Disarm edildi")
+
+land(0, 0) # 0 metre yükseklikte 0 hýzla iniþ yap
+
+# MAV_CMD_NAV_VTOL_LAND komutu ile IHA'nýn iniþ yapmasý saðlanýr
